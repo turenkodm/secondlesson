088e561ca38c73e11524d11c4e098efda98ba4d5
dmitrij <turenkodm@gmail.com>
2021-12-24 11:04:03 +0300

    final_1

--- lib/main.dart
+++ lib/main.dart
@@ -28,97 +28,99 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 80,
-              ),
-              const SizedBox(
-                width: 110,
-                height: 84,
-                child: Image(
-                  image: AssetImage('assets/images/logo.png'),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const Text(
-                'Введите логин в виде 10 цифр номера телефона',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Color.fromRGBO(0, 0, 0, 0.6),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const SizedBox(
-                width: 244,
-                child: TextField(
-                  decoration: InputDecoration(
-                    focusedBorder: borderStyle,
-                    enabledBorder: borderStyle,
-                    filled: true,
-                    fillColor: Color(0xFFeceff1),
-                    labelText: 'Телефон',
+          child: Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 50.0,
+            ),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 80,
                   ),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const SizedBox(
-                width: 244,
-                child: TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    focusedBorder: borderStyle,
-                    enabledBorder: borderStyle,
-                    border: InputBorder.none,
-                    filled: true,
-                    fillColor: Color(0xFFeceff1),
-                    labelText: 'Пароль',
+                  const SizedBox(
+                    width: 110,
+                    height: 84,
+                    child: Image(
+                      image: AssetImage('assets/images/logo.png'),
+                    ),
                   ),
-                ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text(
+                    'Введите логин в виде 10 цифр номера телефона',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Color.fromRGBO(0, 0, 0, 0.6),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const TextField(
+                    keyboardType: TextInputType.phone,
+                    decoration: InputDecoration(
+                      focusedBorder: borderStyle,
+                      enabledBorder: borderStyle,
+                      filled: true,
+                      fillColor: Color(0xFFeceff1),
+                      labelText: 'Телефон',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      focusedBorder: borderStyle,
+                      enabledBorder: borderStyle,
+                      border: InputBorder.none,
+                      filled: true,
+                      fillColor: Color(0xFFeceff1),
+                      labelText: 'Пароль',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 28,
+                  ),
+                  SizedBox(
+                    width: 154,
+                    height: 42,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          primary: const Color(0xFF0079D0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(22),
+                          )),
+                      onPressed: () {},
+                      child: const Text('Войти'),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 62,
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: const Text(
+                      'Регистрация',
+                      style: textStyle,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: const Text(
+                      'Забыли пароль?',
+                      style: textStyle,
+                    ),
+                  ),
+                ],
               ),
-              const SizedBox(
-                height: 28,
-              ),
-              SizedBox(
-                width: 154,
-                height: 42,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      primary: const Color(0xFF0079D0),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(22),
-                      )),
-                  onPressed: () {},
-                  child: const Text('Войти'),
-                ),
-              ),
-              const SizedBox(
-                height: 62,
-              ),
-              InkWell(
-                onTap: () {},
-                child: const Text(
-                  'Регистрация',
-                  style: textStyle,
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              InkWell(
-                onTap: () {},
-                child: const Text(
-                  'Забыли пароль?',
-                  style: textStyle,
-                ),
-              ),
-            ],
+            ),
           ),
         ),
       ),
